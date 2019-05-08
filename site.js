741702d7c95c461e8626d3d7caf54dd2f30a503a
Edwin Sanchez <es269@njit.edu>
2019-05-08 12:40:26 -0400

    Update site.js

--- site.js
+++ site.js
@@ -41,48 +41,7 @@ const vue_app = new Vue({
       },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-            function makeTextDate(dateArray) {
-                  var datestr = "";
-                  if(dateArray[1]==1){
-                        datestr+="January ";
-                  }else if(dateArray[1]==2){
-                        datestr+="February ";
-                  }else if(dateArray[1]==3){
-                        datestr+="March ";
-                  }else if(dateArray[1]==4){
-                        datestr+="April ";
-                  }else if(dateArray[1]==5){
-                        datestr+="May ";
-                  }else if(dateArray[1]==6){
-                        datestr+="June ";
-                  }else if(dateArray[1]==7){
-                        datestr+="July ";
-                  }else if(dateArray[1]==8){
-                        datestr+="August ";
-                  }else if(dateArray[1]==9){
-                        datestr+="September ";
-                  }else if(dateArray[1]==10){
-                        datestr+="October ";
-                  }else if(dateArray[1]==11){
-                        datestr+="November ";
-                  }else{
-                        datestr+="December ";
-                  }
-                  datestr+= dateArray[2]+", "+dateArray[0];
-                  return datestr;
-            },
-            function like(index) {
-                  return index+1;
-            },
-            function dislike(index) {
-                  return index-1;
-            },
             
-            function timeText(minutes) {
-                  var timestr="";
-                  timestr+= Math.floor(minutes/60)+"h "+minutes%60+"m";
-                  return timestr;
-            }
       }
 })
 	
